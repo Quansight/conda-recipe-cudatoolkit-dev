c0507d9c4234a79e4f20d67a7ae66aa0d1309cdf
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-07-18 12:19:09 -0400

    add set_chmod function

--- src/build.py
+++ src/build.py
@@ -2,6 +2,13 @@ from __future__ import print_function
 import os
 import shutil
 from pathlib import Path
+import stat
+
+
+def set_chmod(file_name):
+    # Do a simple chmod +x for a file within python
+    st = os.stat(file_name)
+    os.chmod(file_name, st.st_mode | stat.S_IXOTH)
 
 
 def create_dir(new_dir):
@@ -14,6 +21,7 @@ def create_dir(new_dir):
 def copy_files(src, dst):
     try:
         if (os.path.isfile(src)):
+            set_chmod(src)
             shutil.copy(src, dst)
     except FileExistsError:
         pass
@@ -24,7 +32,7 @@ def _main():
     prefix_dir_path = Path(os.environ['PREFIX'])
     prefix_bin_dir_path = prefix_dir_path / 'bin'
     conda_prefix_path = Path(os.environ['CONDA_PREFIX'])
-    
+    print('---------------', prefix_dir_path)
     print('---------------', conda_prefix_path)
     # Create activated.d and deactivate.d directories
     activate_dir_path = conda_prefix_path / 'etc' / 'conda' / 'activate.d'
