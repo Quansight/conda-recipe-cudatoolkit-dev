4cc699e36fdacff5b77bef4a24da4d2b9252ea10
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-07-18 22:50:27 -0400

    update shebang

--- condarecipe9.2/scripts/activate/cudatoolkit-dev-activate.sh
+++ condarecipe9.2/scripts/activate/cudatoolkit-dev-activate.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/usr/bin/env bash
 python $CONDA_PREFIX/etc/conda/activate.d/cudatoolkit-dev-activate.py
--- condarecipe9.2/scripts/deactivate/cudatoolkit-dev-deactivate.sh
+++ condarecipe9.2/scripts/deactivate/cudatoolkit-dev-deactivate.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/usr/bin/env bash
 python $CONDA_PREFIX/etc/conda/deactivate.d/cudatoolkit-dev-deactivate.py
