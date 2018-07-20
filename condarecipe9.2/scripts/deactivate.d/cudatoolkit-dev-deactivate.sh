5eee84d5bc704b478e1de46afa39afa55c7d8ac4
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-07-20 10:44:39 -0400

    Add symlinks to nvvm libraries

--- condarecipe9.2/scripts/deactivate.d/cudatoolkit-dev-deactivate.sh
+++ condarecipe9.2/scripts/deactivate.d/cudatoolkit-dev-deactivate.sh
@@ -21,4 +21,23 @@ do
     
 done
 
-unset CUDA_HOME
+
+for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/nvvm/bin/*;
+do  
+    to_unlink=$(basename ${f});
+
+    if [ -L $CONDA_PREFIX/bin/${to_unlink} ]; then
+       unlink $CONDA_PREFIX/bin/${to_unlink};
+    fi 
+    
+done
+
+for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/nvvm/lib64/*;
+do  
+    to_unlink=$(basename ${f});
+
+    if [ -L $CONDA_PREFIX/lib/${to_unlink} ]; then
+       unlink $CONDA_PREFIX/lib/${to_unlink};
+    fi 
+    
+done
