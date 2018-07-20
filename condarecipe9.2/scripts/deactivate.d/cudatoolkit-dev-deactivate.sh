3ce5fa68393475b661c76cb90ed4cf4c985fd9ef
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-07-20 17:26:02 -0400

    fix too many arguments error

--- condarecipe9.2/scripts/deactivate.d/cudatoolkit-dev-deactivate.sh
+++ condarecipe9.2/scripts/deactivate.d/cudatoolkit-dev-deactivate.sh
@@ -4,7 +4,7 @@ for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/bin/*;
 do  
     to_unlink=$(basename ${f});
     
-    if [ -L $CONDA_PREFIX/bin/${to_unlink} ]; then
+    if [ -L "$CONDA_PREFIX/bin/${to_unlink}" ]; then
         unlink $CONDA_PREFIX/bin/${to_unlink};
     fi 
 
@@ -15,7 +15,7 @@ for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/lib64/*;
 do  
     to_unlink=$(basename ${f});
 
-    if [ -L $CONDA_PREFIX/lib/${to_unlink} ]; then
+    if [ -L "$CONDA_PREFIX/lib/${to_unlink}" ]; then
        unlink $CONDA_PREFIX/lib/${to_unlink};
     fi 
     
@@ -26,7 +26,7 @@ for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/nvvm/bin/*;
 do  
     to_unlink=$(basename ${f});
 
-    if [ -L $CONDA_PREFIX/bin/${to_unlink} ]; then
+    if [ -L "$CONDA_PREFIX/bin/${to_unlink}" ]; then
        unlink $CONDA_PREFIX/bin/${to_unlink};
     fi 
     
@@ -36,7 +36,7 @@ for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/nvvm/lib64/*;
 do  
     to_unlink=$(basename ${f});
 
-    if [ -L $CONDA_PREFIX/lib/${to_unlink} ]; then
+    if [ -L "$CONDA_PREFIX/lib/${to_unlink}" ]; then
        unlink $CONDA_PREFIX/lib/${to_unlink};
     fi 
     
@@ -46,7 +46,7 @@ for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/nvvm/libdevice/*;
 do  
     to_unlink=$(basename ${f});
 
-    if [ -L $CONDA_PREFIX/lib/${to_unlink} ]; then
+    if [ -L "$CONDA_PREFIX/lib/${to_unlink}" ]; then
        unlink $CONDA_PREFIX/lib/${to_unlink};
     fi 
     
