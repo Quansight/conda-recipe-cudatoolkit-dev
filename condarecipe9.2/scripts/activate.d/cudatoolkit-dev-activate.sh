eb4d5befd4c9a9a4d67fd77e5b20093bbec5957b
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-07-23 13:00:43 -0400

    update symlinks

--- condarecipe9.2/scripts/activate.d/cudatoolkit-dev-activate.sh
+++ condarecipe9.2/scripts/activate.d/cudatoolkit-dev-activate.sh
@@ -14,30 +14,13 @@ do
 
 done
 
-for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/nvvm/bin/*;
-do 
-    link=$(basename "$f");
-    ln -sf $f $CONDA_PREFIX/bin/${link};
-
-done
-
-for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/nvvm/lib64/*;
-do 
-    link=$(basename "$f");
-    ln -sf $f $CONDA_PREFIX/lib/${link};
-
-done
-
-for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/nvvm/libdevice/*;
-do 
-    link=$(basename "$f");
-    ln -sf $f $CONDA_PREFIX/lib/${link};
-
-done
-
 for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/include/*;
 do 
     link=$(basename "$f");
     ln -sf $f $CONDA_PREFIX/include/${link};
 
 done
+
+
+ln -sf $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/nvvm $CONDA_PREFIX/
+ln -sf $CONDA_PREFIX/lib $CONDA_PREFIX/lib64 
--- condarecipe9.2/scripts/deactivate.d/cudatoolkit-dev-deactivate.sh
+++ condarecipe9.2/scripts/deactivate.d/cudatoolkit-dev-deactivate.sh
@@ -22,36 +22,6 @@ do
 done
 
 
-for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/nvvm/bin/*;
-do  
-    to_unlink=$(basename ${f});
-
-    if [ -L "$CONDA_PREFIX/bin/${to_unlink}" ]; then
-       unlink $CONDA_PREFIX/bin/${to_unlink};
-    fi 
-    
-done
-
-for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/nvvm/lib64/*;
-do  
-    to_unlink=$(basename ${f});
-
-    if [ -L "$CONDA_PREFIX/lib/${to_unlink}" ]; then
-       unlink $CONDA_PREFIX/lib/${to_unlink};
-    fi 
-    
-done
-
-for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/nvvm/libdevice/*;
-do  
-    to_unlink=$(basename ${f});
-
-    if [ -L "$CONDA_PREFIX/lib/${to_unlink}" ]; then
-       unlink $CONDA_PREFIX/lib/${to_unlink};
-    fi 
-    
-done
-
 for f in $CONDA_PREFIX/pkgs/cudatoolkit-dev-9.2-0/include/*;
 do 
     to_unlink=$(basename ${f});
@@ -60,3 +30,13 @@ do
        unlink $CONDA_PREFIX/include/${to_unlink};
     fi 
 done    
+
+
+if [ -L "$CONDA_PREFIX/nvvm" ]; then 
+    unlink $CONDA_PREFIX/nvmm;
+fi 
+
+
+if [ -L "$CONDA_PREFIX/lib64" ]; then 
+    unlink $CONDA_PREFIX/lib64;
+fi 
